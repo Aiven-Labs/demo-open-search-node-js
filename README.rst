086b92531161f588eb94bb610bf90fdcf1c3df60
Olena Kutsenko <olena.kutsenko@aiven.io>
2021-09-30 19:42:55 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,32 @@
+Aiven Developer
+===============
+
+This repository contains code examples from the tutorial on `how to use OpenSearch with NodeJS <https://developer.aiven.io/docs/products/opensearch/howto/get-started-with-nodejs.html#query-the-data>`_.
+
+Running locally
+---------------
+
+1. Clone the repository and install the dependencies::
+
+    npm install
+
+2. Copy .env.example, rename to .env and there the service_uri of your OpenSearch cluster.
+
+3. Download full_format_recipes from `Kaggle <https://www.kaggle.com/hugodarwood/epirecipes?select=full_format_recipes.json>`_. Unzip and copy the JSON file to the root folder of this project.
+
+4. Install ``run-func`` to run javascript methods from command line with more convenience::
+
+    npm i -g run-func
+
+You're all set! Retrieve the list of available indices by running
+
+::
+
+    run-func index.js getExistingIndices
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,272 @@
+require("dotenv").config();
+const { Client } = require("@opensearch-project/opensearch");
+
+// full_format_recipes.json taken from
+// https://www.kaggle.com/hugodarwood/epirecipes?select=full_format_recipes.json
+const recipes = require("./full_format_recipes.json");
+
+const client = new Client({
+  node: process.env.SERVICE_URI,
+});
+
+const indexName = "recipes";
+
+/**
+ * Logging result body, used in callbacks.
+ */
+const logBody = (error, result) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result.body);
+  }
+};
+
+/**
+ * Parsing and logging list of titles from the result, used in callbacks.
+ */
+const logTitles = (error, result) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const hits = result.body.hits.hits;
+    console.log(`Number of returned results is ${hits.length}`);
+    console.log(hits.map((hit) => hit._source.title));
+  }
+};
+
+/**
+ * Getting existing indices in the cluster.
+ */
+module.exports.getExistingIndices = () => {
+  console.log(`Getting existing indices:`);
+  client.cat.indices({ format: "json" }, logBody);
+};
+
+/**
+ * Indexing data from json file with recipes.
+ */
+module.exports.indexData = () => {
+  console.log(`Ingesting data: ${recipes.length} recipes`);
+  const body = recipes.flatMap((doc) => [
+    { index: { _index: indexName } },
+    doc,
+  ]);
+
+  client.bulk({ refresh: true, body }, logBody);
+};
+
+/**
+ * Retrieving mapping for the index.
+ */
+module.exports.getMapping = () => {
+  console.log(`Retrieving mapping for the index with name ${indexName}`);
+
+  client.indices.getMapping({ index: indexName }, (error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(result.body.recipes.mappings.properties);
+    }
+  });
+};
+
+/**
+ * Example of using the Lucene query string syntax
+ */
+module.exports.qSearch = () => {
+  client.search(
+    {
+      index: "recipes",
+      q: "ingredients:broccoli AND calories:(>=100 AND <200)",
+    },
+    logTitles
+  );
+};
+
+/**
+ * Searching for exact matches of a value in a field.
+ */
+module.exports.termSearch = (field, value) => {
+  console.log(`Searching for values in the field ${field} equal to ${value}`);
+  const body = {
+    query: {
+      term: {
+        [field]: value,
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Searching for exact matches of a value in a field.
+ */
+module.exports.rangeSearch = (field, gte, lte) => {
+  console.log(
+    `Searching for values in the ${field} ranging from ${gte} to ${lte}`
+  );
+  const body = {
+    query: {
+      range: {
+        [field]: {
+          gte,
+          lte,
+        },
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Specifying fuzziness to account for typos and misspelling.
+ */
+module.exports.fuzzySearch = (field, value, fuzziness) => {
+  console.log(
+    `Search for ${value} in the ${field} with fuzziness set to ${fuzziness}`
+  );
+  const query = {
+    query: {
+      fuzzy: {
+        [field]: {
+          value,
+          fuzziness,
+        },
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body: query,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Finding matches sorted by relevance.
+ */
+module.exports.matchSearch = (field, query) => {
+  console.log(`Searching for ${query} in the field ${field}`);
+  const body = {
+    query: {
+      match: {
+        [field]: {
+          query,
+        },
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Specifying a slop - a distance between search words.
+ */
+module.exports.slopSearch = (field, query, slop) => {
+  console.log(
+    `Searching for ${query} with slop value ${slop} in the field ${field}`
+  );
+  const body = {
+    query: {
+      match_phrase: {
+        [field]: {
+          query,
+          slop,
+        },
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Using special operators within a query string and a size parameter.
+ */
+module.exports.querySearch = (field, query, size) => {
+  console.log(
+    `Searching for ${query} in the field ${field} and returning maximum ${size} results`
+  );
+  const body = {
+    query: {
+      query_string: {
+        default_field: field,
+        query,
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+      size,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Combining several queries together
+ */
+module.exports.booleanSearch = () => {
+  console.log(
+    `Searching for quick and easy recipes without garlic with low sodium and high protein`
+  );
+  const body = {
+    query: {
+      bool: {
+        must: { match: { categories: "Quick & Easy" } },
+        must_not: { match: { ingredients: "garlic" } },
+        filter: [
+          { range: { sodium: { lte: 50 } } },
+          { range: { protein: { gte: 5 } } },
+        ],
+      },
+    },
+  };
+  client.search(
+    {
+      index: indexName,
+      body,
+    },
+    logTitles
+  );
+};
+
+/**
+ * Deleting the index
+ */
+module.exports.deleteIndex = () => {
+  client.indices.delete(
+    {
+      index: indexName,
+    },
+    logBody
+  );
+};
